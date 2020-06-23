c75896dd8beb0a7d16bb6c11c9b6b33f236b891f
Summer Bossman <summer.bossman@gmail.com>
2020-06-23 19:15:39 +0000

    Update PlayExample.java

--- examples/PlayExample.java
+++ examples/PlayExample.java
@@ -3,7 +3,7 @@ import classes.Play;
 
 public class PlayExample{
 	public static void main(String[] args) throws FileNotFoundException{
-		Play play = new Play("Hamlet", "C:\\Users\\summe\\eclipse-workspace\\EquitableLineDistribution\\hamlet.txt");
+		Play play = new Play("Hamlet", "C:\\Users\\summe\\eclipse-workspace\\EquitableLineDistribution\\plays\\hamlet.txt");
 		
 		System.out.println("The play " + play.getTitle() + "'s cast is: " + play.getCast());
 		System.out.println("The play's script is: " + play.getScript());
